48674f70e0852e6d176f872aea79f09e1761c131
plumhead <andy@plumhead-software.com>
2016-05-17 16:52:10 +0100

    Added basic UI

---
+++ PagingTextExample/PagingTextExample/PageViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PageViewCell.swift
+//  PagingTextExample
+//
+//  Created by Developer on 17/05/2016.
+//  Copyright © 2016 Plumhead Software. All rights reserved.
+//
+
+import UIKit
+
+class PageViewCell: UICollectionViewCell {
+    @IBOutlet weak var pageLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- PagingTextExample/PagingTextExample/ViewController.swift
+++ PagingTextExample/PagingTextExample/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var editor: UITextView!
+    @IBOutlet weak var pageCollection: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
+
+//MARK: - Page Data Source
+extension ViewController : UICollectionViewDataSource {
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = pageCollection.dequeueReusableCellWithReuseIdentifier("IdPageCell", forIndexPath: indexPath) as! PageViewCell
+        
+        cell.pageLabel.text = "page: \(indexPath.row + 1)"
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10 // replace with real page count
+    }
+}
+
+
+//MARK: - Page Delegate
+extension ViewController : UICollectionViewDelegate {
+    
+}
+
 
